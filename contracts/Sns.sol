6b1822bc2141996caeb3f7093aa1ea23c8061dac
Rex <sns.net@protonmail.com>
2024-10-21 04:15:40 -0700

    fix: Rename initialOwner to owner

--- contracts/Sns.sol
+++ contracts/Sns.sol
@@ -8,21 +8,13 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 // SNS token contract inheriting from ERC20 for the basic token functionality, ERC20Permit for gasless approvals, and Ownable to handle ownership-related features.
 
 contract SNSToken is ERC20, Ownable, ERC20Permit {
-    // Event to be emitted when tokens are burned
-    event Burn(address indexed burner, uint256 amount);
     // Event to be emitted when tokens are minted
     event Mint(address indexed minter, address indexed recipient, uint256 amount);
+    // Event to be emitted when tokens are burned
+    event Burn(address indexed burner, uint256 amount);
 
     // Constructor to initialize the token with name, symbol, and initial owner
-    constructor(
-        address initialOwner
-    ) ERC20("Sonic Name Service", "SNS") Ownable(initialOwner) ERC20Permit("Sonic Name Service") {}
-
-    // Function to allow token holders to burn their own tokens
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
-        emit Burn(msg.sender, amount);
-    }
+    constructor(address owner) ERC20("Sonic Name Service", "SNS") Ownable(owner) ERC20Permit("Sonic Name Service") {}
 
     /*
     Function to mint new tokens, restricted to the owner only.
@@ -34,5 +26,11 @@ contract SNSToken is ERC20, Ownable, ERC20Permit {
         require(to != address(0), "Mint to zero address!");
         _mint(to, amount);
         emit Mint(msg.sender, to, amount);
+    }
+
+    // Function to allow token holders to burn their own tokens
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+        emit Burn(msg.sender, amount);
     }
 }
