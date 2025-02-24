c636c0adde579e225d0f877524cff569c570c4a5
Rex <sns.net@protonmail.com>
2025-02-24 02:44:44 -0800

    New token contract on Sonic

--- contracts/Sns.sol
+++ contracts/Sns.sol
@@ -2,32 +2,25 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-// SNS token contract inheriting from ERC20 for the basic token functionality, ERC20Permit for gasless approvals, and Ownable to handle ownership-related features.
+// SNS token contract inheriting from ERC20 for the basic token functionality, ERC20Permit for gasless approvals.
 
-contract SNSToken is ERC20, Ownable, ERC20Permit {
+contract SNSToken is ERC20, ERC20Permit {
     // Event to be emitted when tokens are minted
-    event Mint(address indexed minter, address indexed recipient, uint256 amount);
+    event Mint(address indexed recipient, uint256 amount);
     // Event to be emitted when tokens are burned
     event Burn(address indexed burner, uint256 amount);
 
     // Constructor to initialize the token with name, symbol, and initial owner
     constructor(
-        address initialOwner
-    ) ERC20("Sonic Name Service", "SNS") Ownable(initialOwner) ERC20Permit("Sonic Name Service") {}
+        address initialOwner,
+        uint256 migratedAmount
+    ) ERC20("Sonic Name Service", "SNS") ERC20Permit("Sonic Name Service") {
+        require(initialOwner != address(0), "Initial Owner address cannot be zero");
+        _mint(initialOwner, migratedAmount);
 
-    /*
-    Function to mint new tokens, restricted to the owner only.
-    After renouncing ownership of the SNS token, minting new SNS tokens will no longer be possible.
-    This ensures that no additional tokens can be minted once the burn-to-mint process is finalized, 
-    providing trust in the token's supply cap.
-    */
-    function mint(address recipient, uint256 amount) external onlyOwner {
-        require(recipient != address(0), "Mint to zero address!");
-        _mint(recipient, amount);
-        emit Mint(msg.sender, recipient, amount);
+        emit Mint(initialOwner, migratedAmount);
     }
 
     // Function to allow token holders to burn their own tokens
